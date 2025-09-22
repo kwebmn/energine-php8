a09b358a4edc0c345fadeac12ec059023e4e0b6c
kwebmn <vitaly.yuzvishen@gmail.com>
2025-09-22 09:10:49 +0000

    some fixes

---
+++ system.jsmap.php
@@ -0,0 +1,111 @@
+<?php return array (
+  'TagEditor' => 
+  array (
+    0 => 'GridManager',
+  ),
+  'AttachmentEditor' => 
+  array (
+    0 => 'GridManager',
+    1 => 'FileAPI/FileAPI',
+  ),
+  'FileRepoForm' => 
+  array (
+    0 => 'Form',
+    1 => 'FileAPI/FileAPI',
+  ),
+  'DivManager' => 
+  array (
+    0 => 'TabPane',
+    1 => 'Toolbar',
+    2 => 'ModalBox',
+    3 => 'jquery.min',
+    4 => 'jstree/jstree',
+  ),
+  'TemplateWizard' => 
+  array (
+    0 => 'GridManager',
+  ),
+  'GroupForm' => 
+  array (
+    0 => 'Form',
+  ),
+  'UserManager' => 
+  array (
+    0 => 'GridManager',
+  ),
+  'DivSidebar' => 
+  array (
+    0 => 'DivManager',
+    1 => 'jquery.min',
+    2 => 'jstree/jstree',
+  ),
+  'ValidForm' => 
+  array (
+    0 => 'Validator',
+  ),
+  'GridManager' => 
+  array (
+    0 => 'TabPane',
+    1 => 'PageList',
+    2 => 'Toolbar',
+    3 => 'ModalBox',
+  ),
+  'DivTree' => 
+  array (
+    0 => 'DivManager',
+    1 => 'jquery.min',
+    2 => 'jstree/jstree',
+  ),
+  'ImageManager' => 
+  array (
+    0 => 'Form',
+    1 => 'ModalBox',
+  ),
+  'SiteManager' => 
+  array (
+    0 => 'GridManager',
+  ),
+  'FeedToolbar' => 
+  array (
+    0 => 'Toolbar',
+    1 => 'ModalBox',
+  ),
+  'FiltersTreeEditor' => 
+  array (
+    0 => 'ModalBox',
+    1 => 'jquery.min',
+    2 => 'jstree/jstree.min',
+  ),
+  'PageToolbar' => 
+  array (
+    0 => 'Toolbar',
+    1 => 'ModalBox',
+    2 => 'Cookie',
+  ),
+  'FileRepository' => 
+  array (
+    0 => 'GridManager',
+    1 => 'Cookie',
+    2 => 'FileAPI/FileAPI',
+  ),
+  'DivForm' => 
+  array (
+    0 => 'Form',
+    1 => 'ModalBox',
+  ),
+  'PageEditor' => 
+  array (
+    0 => 'ckeditor/ckeditor',
+    1 => 'ModalBox',
+  ),
+  'Form' => 
+  array (
+    0 => 'ckeditor/ckeditor',
+    1 => 'TabPane',
+    2 => 'Toolbar',
+    3 => 'Validator',
+    4 => 'ModalBox',
+    5 => 'AcplField',
+    6 => 'Cookie',
+  ),
+);
