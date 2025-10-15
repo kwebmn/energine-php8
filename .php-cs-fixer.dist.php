f750686023ed2f4dc009c764255f9dbe9ea7f2d4
kwebmn <vitaly.yuzvishen@gmail.com>
2025-10-15 16:47:46 +0300

    Refine PHP-CS-Fixer brace and continuation rules

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -16,28 +16,28 @@ $finder = PhpCsFixer\Finder::create()
 return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
-        '@PSR12' => true,
+        '@PSR12' => true, // останется, но ниже мы переопределяем то, что нам нужно
 
-        // Скобки на новой строке
+        // 1) { на новой строке у конструкций управления
         'braces_position' => [
-            'classes_opening_brace' => 'next_line_unless_newline_at_signature_end',
-            'functions_opening_brace' => 'next_line_unless_newline_at_signature_end',
             'control_structures_opening_brace' => 'next_line_unless_newline_at_signature_end',
-            'anonymous_classes_opening_brace' => 'next_line_unless_newline_at_signature_end',
+            // если хочешь Allman-стиль «везде», добавь ещё:
+            'functions_opening_brace' => 'next_line_unless_newline_at_signature_end',
+            'classes_opening_brace' => 'next_line_unless_newline_at_signature_end',
             'anonymous_functions_opening_brace' => 'next_line_unless_newline_at_signature_end',
-            // Чтобы анонимные функции/пустые анонимные классы не оставались в одну строку:
+            'anonymous_classes_opening_brace' => 'next_line_unless_newline_at_signature_end',
             'allow_single_line_anonymous_functions' => false,
             'allow_single_line_empty_anonymous_classes' => false,
         ],
 
-        // Твои текущие правила
+        // 2) else/catch/finally на новой строке
+        'control_structure_continuation_position' => ['position' => 'next_line'],
+
+        // твои правила
         'nullable_type_declaration_for_default_null_value' => true,
         'array_syntax' => ['syntax' => 'short'],
         'ordered_imports' => true,
         'no_unused_imports' => true,
         'single_quote' => true,
-
-        // (опционально) без пустой строки сразу после {
-        'no_blank_lines_after_class_opening' => true,
     ])
     ->setFinder($finder);
