36f37c69945f179aeda4181785de33af442d469e
kwebmn <vitaly.yuzvishen@gmail.com>
2025-10-15 17:37:47 +0300

    Align DefaultTemplateFeed defineParams signature

--- engine/core/modules/wizard/components/DefaultTemplateFeed.class.php
+++ engine/core/modules/wizard/components/DefaultTemplateFeed.class.php
@@ -8,7 +8,7 @@ class DefaultTemplateFeed extends ExtendedFeed
         $this->setTableName('DefaultTemplateTableName');
     }
 
-    protected function defineParams()
+    protected function defineParams(): array
     {
         $result = array_merge(
             parent::defineParams(),
