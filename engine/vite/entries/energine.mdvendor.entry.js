2db024c571af3266ca636320445e2b5c06d266d0
kwebmn <vitaly.yuzvishen@gmail.com>
2025-10-28 20:35:45 +0200

    Ensure MDB UI kit exposes global namespace

--- engine/vite/entries/energine.mdvendor.entry.js
+++ engine/vite/entries/energine.mdvendor.entry.js
@@ -1,3 +1,10 @@
 import './styles/mdvendor.css';
 import 'mdb-ui-kit/css/mdb.min.css';
-import 'mdb-ui-kit/js/mdb.es.min.js';
+
+import mdb from 'mdb-ui-kit/js/mdb.umd.min.js';
+
+if (typeof window !== 'undefined' && mdb && !window.mdb) {
+    window.mdb = mdb;
+}
+
+export default mdb;
