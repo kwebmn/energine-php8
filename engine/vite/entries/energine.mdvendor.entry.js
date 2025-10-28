d997415909cf224ebf73d9f45116e2d92c96b194
kwebmn <vitaly.yuzvishen@gmail.com>
2025-10-28 08:11:15 +0200

    Restore MDB ESM bundle and expose globals

--- engine/vite/entries/energine.mdvendor.entry.js
+++ engine/vite/entries/energine.mdvendor.entry.js
@@ -1,9 +1,15 @@
 import './styles/mdvendor.css';
 import 'mdb-ui-kit/css/mdb.min.css';
-import 'mdb-ui-kit/js/mdb.umd.min.js';
+import mdb from 'mdb-ui-kit/js/mdb.es.min.js';
 
-if (typeof window !== 'undefined'
-    && typeof window.bootstrap === 'undefined'
-    && typeof window.mdb !== 'undefined') {
-    window.bootstrap = window.mdb;
+if (typeof window !== 'undefined') {
+    if (typeof window.mdb === 'undefined') {
+        window.mdb = mdb;
+    }
+
+    if (typeof window.bootstrap === 'undefined' && typeof window.mdb !== 'undefined') {
+        window.bootstrap = window.mdb;
+    }
 }
+
+export default mdb;
