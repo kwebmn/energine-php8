8af83d3c6c1e0e02613772214a1192a993078227
kwebmn <vitaly.yuzvishen@gmail.com>
2025-10-28 20:58:41 +0200

    Use namespace MDB import for global exposure

--- engine/vite/entries/energine.mdvendor.entry.js
+++ engine/vite/entries/energine.mdvendor.entry.js
@@ -1,7 +1,7 @@
 import './styles/mdvendor.css';
 import 'mdb-ui-kit/css/mdb.min.css';
 
-import mdb from 'mdb-ui-kit/js/mdb.umd.min.js';
+import * as mdb from 'mdb-ui-kit/js/mdb.umd.min.js';
 
 if (typeof window !== 'undefined' && mdb && !window.mdb) {
     window.mdb = mdb;
