677764e72deecde9af85b50aae10c33656d17429
kwebmn <vitaly.yuzvishen@gmail.com>
2025-10-28 09:03:02 +0200

    Add missing extended CSS bundle imports

--- engine/vite/entries/energine.extended.entry.js
+++ engine/vite/entries/energine.extended.entry.js
@@ -1,3 +1,5 @@
+import './styles/extended.css';
+
 import 'engine/core/modules/share/scripts/AcplField.js';
 import 'engine/core/modules/share/scripts/AttachmentEditor.js';
 import 'engine/core/modules/share/scripts/Cookie.js';
