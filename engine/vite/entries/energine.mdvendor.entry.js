ebc068f1f04cc93720294022f2c4f3ea0db3a120
kwebmn <vitaly.yuzvishen@gmail.com>
2025-10-28 07:50:31 +0200

    Fix MDBootstrap globals for toolbar widgets

--- engine/vite/entries/energine.mdvendor.entry.js
+++ engine/vite/entries/energine.mdvendor.entry.js
@@ -1,3 +1,9 @@
 import './styles/mdvendor.css';
 import 'mdb-ui-kit/css/mdb.min.css';
-import 'mdb-ui-kit/js/mdb.es.min.js';
+import 'mdb-ui-kit/js/mdb.umd.min.js';
+
+if (typeof window !== 'undefined'
+    && typeof window.bootstrap === 'undefined'
+    && typeof window.mdb !== 'undefined') {
+    window.bootstrap = window.mdb;
+}
